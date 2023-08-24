a0ad06f006fa058770193fd6db0859a63ff085fa
YeGoTaChi <yego.designs@gmail.com>
2023-08-24 19:23:05 +0100

    Refactored Code

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: ylyoussf <ylyoussf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 04:49:35 by ylyoussf          #+#    #+#             */
-/*   Updated: 2023/08/24 15:38:35 by ylyoussf         ###   ########.fr       */
+/*   Updated: 2023/08/24 19:09:48 by ylyoussf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,30 @@ void print_token(void *token) // Debug !
 	printf("\n");
 }
 
+void	get_token(char *where, t_token *tok)
+{
+	tok->type = get_token_type(where);
+	tok->len = get_token_len(tok->type, where);
+}
+
+bool	check_tok(t_token *this_tok, char *cmd_line,
+		int index_space[2], t_token **tokens)
+{
+	if (this_tok->type < 0)
+		return (tok_error(cmd_line[index_space[0]]), false);
+	if (this_tok->type == WHITE_SPACE)
+		return (true);
+	if (this_tok->len == 1 &&
+		(STR <= this_tok->type && this_tok->type <= DQSTR))
+		return (unclosed_error(cmd_line[index_space[0]]), false);
+	this_tok->value = ft_substr(cmd_line, index_space[0], this_tok->len);
+	if (!this_tok->value)
+		return (false);
+	add_token(tokens,
+		new_token(this_tok->type, this_tok->value, this_tok->len)
+		, index_space[1]);
+	return (true);
+}
 
 bool	lexer(char *command_line, t_token **tokens)
 {
@@ -95,25 +119,10 @@ bool	lexer(char *command_line, t_token **tokens)
 	while (command_line[index])
 	{
 		space = this_tok.type == WHITE_SPACE;
-		this_tok.type = get_token_type(command_line + index);
-		this_tok.len = get_token_len(this_tok.type, command_line + index);
-		if ((int)this_tok.type >= 0 && this_tok.type != WHITE_SPACE)
-		{
-			if (this_tok.len == 1 && (STR <= this_tok.type && this_tok.type <= DQSTR))
-				return (free_tok_lst(*tokens), unclosed_error(command_line[index]), false);
-			this_tok.value = ft_substr(command_line, index, this_tok.len);
-			if (!this_tok.value)
-				return (false);
-			add_token(tokens,
-				new_token(this_tok.type, this_tok.value, this_tok.len)
-				, space);
-		}
-		else if (this_tok.type != WHITE_SPACE)
-		{
-			tok_error(command_line[index]);
+		get_token(command_line + index, &this_tok);
+		if (!check_tok(&this_tok, command_line,
+				(int [2]){index, space}, tokens))
 			return (free_tok_lst(*tokens), false);
-			// printf("[%s] %c\n", "UNKNOWN", command_line[index]); // Debug !
-		}
 		index += this_tok.len;
 	}
 	ft_tokadd_back(tokens, new_token(NEW_LINE, ft_strdup("newline"), 7));
