26829307abb4c303f9f9fcb9501fd0d40d31104f
Youssef Lyoussfi <yego.designs@gmail.com>
2023-09-01 03:21:56 +0100

    Remove: Redundency

--- builtins/buitin_utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-# define UTILS_H
-# include <stddef.h>
-
-int	consists_of(char *line , char c);
-size_t	count(const char *s, int c);
-#endif
--- builtins/minishell.h
+++
@@ -1,7 +0,0 @@
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-void builtins(int argc, char *command, char **args,/* char **envp, */t_env **env);
-void echo(int argc, char **args);
-void pwd();
-# endif
