e4a0a7485bf299f506b5753fcea739cf8f5e838e
YeGoTaChi <yego.designs@gmail.com>
2023-10-10 15:42:50 +0100

    Fix: top C-\ tty issue

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ylyoussf <ylyoussf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 19:53:08 by ylyoussf          #+#    #+#             */
-/*   Updated: 2023/10/07 21:03:08 by afatimi          ###   ########.fr       */
+/*   Updated: 2023/10/10 15:36:16 by ylyoussf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 #include <lexer.h>
 #include <parser.h>
 #include <signals.h>
+#include <termios.h>
 
 #define RED "\033[91m"
 #define NOCOL "\033[0m"
+
+enum { 
+	INPUT_AT,
+	OUTPUT_AT,
+	ERROR_AT
+};
 
 void	chk(void)
 {
@@ -32,12 +39,29 @@ void	prompt_pwd()
 	printf("[" RED "%s" NOCOL "]\n", pwd_trolling(NULL));
 }
 
+void	tty_attr(struct termios *attrs, int action)
+{
+	if (action == SET)
+	{
+		tcgetattr(STDIN_FILENO, &attrs[0]);
+		tcgetattr(STDOUT_FILENO, &attrs[1]);
+		tcgetattr(STDERR_FILENO, &attrs[2]);
+	}
+	else if (action == RESET)
+	{
+		tcsetattr(STDIN_FILENO, TCSANOW, &attrs[0]);
+		tcsetattr(STDOUT_FILENO, TCSANOW, &attrs[1]);
+		tcsetattr(STDERR_FILENO, TCSANOW, &attrs[2]);
+	}
+}
+
 int	main(int _, char **__, char **envp)
 {
 	t_token		*tokens;
 	t_ast_cmd	*ast;
 	char		*command_line;
 	char		*tmp;
+	struct termios	attrs[3];
 
 	// TODO : Make an actual Prompt with PWD
 	// atexit(chk); // ? Debug
@@ -49,6 +73,7 @@ int	main(int _, char **__, char **envp)
 	pwd_trolling(tmp);
 	free(tmp);
 	install_default_sig_handlers();
+	tty_attr(attrs, SET);
 	while (true)
 	{
 		if (g_last_signal != 69)
@@ -70,6 +95,7 @@ int	main(int _, char **__, char **envp)
 			add_history(command_line);
 		free_ast(ast);
 		free(command_line);
+		tty_attr(attrs, RESET);
 	}
 	free(command_line);
 	exit(g_exit_status);
