deb242975d2867a4ea63252bbd0ef1355455f493
Youssef Lyoussfi <49578293+YeGoRenji@users.noreply.github.com>
2023-10-16 18:24:30 +0000

    Fix: CTRL-C heredoc leak

---
+++ include/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylyoussf <ylyoussf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 16:14:32 by ylyoussf          #+#    #+#             */
+/*   Updated: 2022/11/16 18:44:23 by ylyoussf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <libft.h>
+
+size_t	ft_strlen_noseg(const char *s);
+char	*get_next_line(int fd);
+
+#endif
--- src/libft/get_next_line.c
+++ src/libft/get_next_line.c
@@ -3,86 +3,104 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afatimi <afatimi@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: ylyoussf <ylyoussf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/05 13:53:28 by afatimi           #+#    #+#             */
-/*   Updated: 2023/10/10 16:40:32 by afatimi          ###   ########.fr       */
+/*   Created: 2022/11/12 03:13:45 by ylyoussf          #+#    #+#             */
+/*   Updated: 2022/11/18 13:50:38 by ylyoussf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <libft.h>
-#include <unistd.h>
 
-static char	*add_to_buff(char *buff, int fd)
+#include <get_next_line.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 69
+#endif
+
+static char	*ft_strjoin_free(char *s1, char *s2)
 {
-	char	*chunk;
-	char	*tmp;
-	int		readed;
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*target;
 
-	chunk = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
-	if (!chunk)
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (s1);
+	s1_len = ft_strlen_noseg(s1);
+	s2_len = ft_strlen_noseg(s2);
+	target = malloc(s1_len + s2_len + 1);
+	if (!target)
 		return (NULL);
-	readed = -1;
-	while (!ft_strchr(chunk, '\n') && readed)
-	{
-		readed = read(fd, chunk, BUFFER_SIZE);
-		if (!readed)
-			break ;
-		if (readed == -1)
-			return (free(chunk), NULL);
-		chunk[readed] = 0;
-		tmp = ft_strjoin(buff, chunk);
-		if (!tmp)
-			return (free(buff), free(chunk), NULL);
-		free(buff);
-		buff = tmp;
-	}
-	return (free(chunk), buff);
+	ft_memcpy(target, s1, s1_len);
+	ft_memcpy(target + s1_len, s2, s2_len);
+	target[s1_len + s2_len] = '\0';
+	return (free(s1), s1 = NULL, target);
 }
 
-static char	*get_line(char **buff)
+static int	ft_read(char **big_buffer, int fd)
 {
-	char	*line;
-	char	*tmp;
-	char	*end_of_line;
-	size_t	line_len;
+	char	*buffer;
+	int		bytes_read;
 
-	if (!*buff)
-		return (NULL);
-	end_of_line = ft_strchr(*buff, '\n');
-	if (!end_of_line)
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (0);
+	bytes_read = -1;
+	while (bytes_read != 0)
 	{
-		if (!ft_strlen(*buff))
-			return (NULL);
-		tmp = ft_strdup(*buff);
-		free(*buff);
-		*buff = NULL;
-		return (tmp);
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (free(buffer), free(*big_buffer), *big_buffer = NULL, 0);
+		buffer[bytes_read] = '\0';
+		*big_buffer = ft_strjoin_free(*big_buffer, buffer);
+		if (ft_strchr(buffer, '\n'))
+			break ;
 	}
-	line_len = end_of_line - *buff + 1;
-	line = ft_substr(*buff, 0, line_len);
-	if (!line)
-		return (free(line), NULL);
-	tmp = ft_strdup(*buff + line_len);
-	free(*buff);
-	*buff = tmp;
-	return (line);
+	free(buffer);
+	return (1);
+}
+
+static char	*get_line(char *big_buffer)
+{
+	char	*new_line_pos;
+
+	new_line_pos = ft_strchr(big_buffer, '\n');
+	if (new_line_pos)
+		return (ft_substr(big_buffer, 0, new_line_pos - big_buffer + 1));
+	return (ft_strdup(big_buffer));
+}
+
+static char	*get_remainder(char *big_buffer)
+{
+	char	*new_line_pos;
+	int		len;
+	int		remainder_index;
+	char	*tmp;
+
+	new_line_pos = ft_strchr(big_buffer, '\n');
+	len = ft_strlen_noseg(big_buffer);
+	remainder_index = new_line_pos - big_buffer + 1;
+	if (new_line_pos)
+	{
+		tmp = ft_substr(big_buffer, remainder_index, len - remainder_index);
+		return (free(big_buffer), big_buffer = NULL, tmp);
+	}
+	free(big_buffer);
+	return (NULL);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buff;
-	char		*line;
+	static char	*big_buffer;
+	char		*actual_line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) == -1)
+		return (free(big_buffer), big_buffer = NULL, NULL);
+	if (!ft_read(&big_buffer, fd))
 		return (NULL);
-	if (!buff)
-		buff = ft_strdup("");
-	buff = add_to_buff(buff, fd);
-	line = get_line(&buff);
-	if (!line)
-	{
-		free(buff);
-		buff = NULL;
-	}
-	return (line);
+	actual_line = get_line(big_buffer);
+	big_buffer = get_remainder(big_buffer);
+	if (*actual_line == '\0')
+		return (free(actual_line), actual_line = NULL, NULL);
+	return (actual_line);
 }
