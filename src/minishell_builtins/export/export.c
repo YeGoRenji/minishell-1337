063764ff297c2fb004840fe00856f63754f43b64
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2023-10-17 15:21:21 +0100

    Fix: export return status

--- src/minishell_builtins/export/export.c
+++ src/minishell_builtins/export/export.c
@@ -3,6 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: afatimi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 15:18:58 by afatimi           #+#    #+#             */
+/*   Updated: 2023/10/17 15:20:52 by afatimi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: afatimi <afatimi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 14:52:58 by afatimi           #+#    #+#             */
@@ -17,23 +29,28 @@ int	export(int argc, char **argv, t_env **env)
 	int		i;
 	char	*ptr;
 	char	*key;
+	int res;
 
 	if (!env)
 		return (1);
 	if (!argc)
 		return (print_exports(*env));
 	i = 0;
+	res = 0;
 	while (i < argc)
 	{
 		ptr = argv[i++];
 		if (validate_arg(ptr))
+		{
+			res = 1;
 			continue ;
+		}
 		if (handle_concate_case(ptr))
 			continue ;
 		key = get_key(ptr);
 		set_env_value(key, get_value(ptr), !!ft_strchr(ptr, '='));
 	}
-	return (0);
+	return (res);
 }
 
 int	handle_concate_case(char *line)
