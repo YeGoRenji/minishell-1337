b2f0de4fa70a74072b5963c05931b9391d981a2c
YeGoTaChi <yego.designs@gmail.com>
2024-02-25 20:37:03 +0100

    Clean: just making project public

--- watcher-mac.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-Xee³ lol.png &
-fswatch -o tree.dot | xargs -n1 -I{} ~/goinfre/Dot-to-Png/index.js 500 tree.dot lol.png &
--- watcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-feh lol.png -R 1 &
-
-while inotifywait -e close_write tree.dot;
-do 
-	dot -Tpng tree.dot > lol.png; 
-done
